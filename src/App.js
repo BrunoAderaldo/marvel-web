53498669db7ca2b069e6391fb00b1c7398a8efec
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-07-01 14:21:41 -0300

    First commit base structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters: [],
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  async getUser() {
+    try {
+      const response = await axios.get(`/characters?orderBy=name&apikey=${process.env.REACT_APP_MARVEL_PUBLIC}`);
+      console.log(response.data);
+      this.setState({ characters: response.data.data.results, count: response.data.data.count });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-
+        <span>{ this.state.count } Characters</span>
+        <ul className="list">
+          {
+            this.state.characters.map(character => (
+              <li className="list-item" key={character.id}>
+                <h5>{character.name}</h5>
+                <h6>{character.id}</h6>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
