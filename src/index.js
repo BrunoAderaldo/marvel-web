611e717a6326d76e3ba6952ad8e1725dcffb96db
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-08-06 23:52:31 -0300

    add feather-icons package

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './App.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import feather from 'feather-icons';
 import HeroInfo from './components/HeroInfo/HeroInfo';
+import Navbar from './components/Navbar/Navbar';
+
+document.addEventListener('DOMContentLoaded', () => {
+  feather.replace();
+});
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={App} />
-      <Route path="/hero/:id" component={HeroInfo} />
-    </Switch>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact={true} component={App} />
+          <Route path="/hero/:id" component={HeroInfo} />
+        </Switch>
+      </div>
+    </React.Fragment>
   </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
